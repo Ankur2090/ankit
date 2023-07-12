c1a42ed5ce80e568c31c6676d4a4f9a949f25174
clavax <clavax@DESKTOP-KU5I7E6>
2023-07-12 21:31:57 +0530

    new add number

---
+++ gitdemo/src/test/java/gitdemo/sumofArray.java
@@ -0,0 +1,22 @@
+package gitdemo;
+
+public class sumofArray {
+
+	public static void main(String[] args) {
+		
+		
+		
+	
+		  int x=45;
+			int y=62;
+			int l = x+y;
+			System.out.println(l);
+		}
+
+	}
+
+	
+
+	
+
+
